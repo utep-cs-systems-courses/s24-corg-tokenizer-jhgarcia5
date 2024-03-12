e1bfa226885be1ccf169ca4bbca3040518a3f922
Jorge Garcia <jhgarcia5@miners.utep.edu>
2024-03-12 11:14:12 -0600

    Uimain now accepts input

--- src/uimain.c
+++ src/uimain.c
@@ -7,39 +7,36 @@ int main()
 
   while(1) {
 
-    fputs("Please input a string to tokenizer ( i ), check history ( h ), or quit ( q )\n>", stdout);
+    fputs("Please WOKR input a string to tokenizer ( i ), check history ( h ), or quit ( q )\n>", stdout);
     fflush(stdout);
-    int c;
-    while(c == '\n'){ //Ignore entered newlines
+    int c = getchar();
+    while(c == '\n' || c == EOF){ //Ignore entered newlines
       c = getchar();
-    }
-  
-    if (c == EOF){
-      goto done;
     }
 	  
     switch(c) {
-    case 'i':
-      puts("Enter your new string");
-      puts(">");
-      char inputString[100];
-      fgets(inputString, sizeof(inputString), stdin);
-      char **tokens = tokenize(inputString);
-      print_tokens(tokens);
-      //HAVE TO ADD TO LLIST IN HISTORY
+      case 'i':
+        fputs("Enter your new string\n>", stdout);
+	fflush(stdout);
+        char inputString[100];
+        fgets(inputString, sizeof(inputString), stdin);
+	printf("You inputed: %s", inputString);
+	char **tokens = tokenize(inputString);
+        print_tokens(tokens);
+        //HAVE TO ADD TO LLIST IN HISTORY
 
-      free_tokens(tokens);
-      break;
+        free_tokens(tokens);
+        break;
 
-    case 'h':
-      break;
+      case 'h':
+        break;
 
-    case 'q':
-      puts("Thank you for using this program!");
-      goto done;
+      case 'q':
+        puts("Thank you for using this program!");
+        goto done;
 
-    default:
-      puts("Please enter a recognized option");
+      default:
+        puts("Please enter a recognized option");
 
     }     
   }
